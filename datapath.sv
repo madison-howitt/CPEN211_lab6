9d9764bbff2eafaa4b99ac070d0714145e106a36
madison-howitt <159974224+madison-howitt@users.noreply.github.com>
2024-11-19 09:56:06 -0800

    Update datapath.sv
    added first 2 components of lab 6

--- datapath.sv
+++ datapath.sv
@@ -1,9 +1,10 @@
-module datapath(datapath_in, vsel, writenum, write, readnum, clk, loada, loadb, shift, asel, bsel, ALUop, loads, loadc, Z_out, datapath_out);
+module datapath(datapath_in, mdata, sximm8, PC, C, sximm5, vsel, writenum, write, readnum, clk, loada, loadb, shift, asel, bsel, ALUop, loads, loadc, Z_out, datapath_out);
   // declare inputs to top-level module
-  input [15:0] datapath_in; 
+  input [15:0] datapath_in, mdata, sximm8, C, sximm5; // new variables for lab 6: mdata, sximm8, PC, C, sximm5
+  input [7:0] PC; 
   input [2:0] writenum, readnum;
-  input [1:0] shift, ALUop; 
-  input vsel, write, clk, loada, loadb, asel, bsel, loads, loadc;  
+  input [1:0] shift, ALUop, vsel; // changed vsel to 2-bit select input
+  input write, clk, loada, loadb, asel, bsel, loads, loadc;  
 
   // declare outputs of top-level module 
   output reg [15:0] datapath_out; 
@@ -22,11 +23,21 @@ module datapath(datapath_in, vsel, writenum, write, readnum, clk, loada, loadb, 
   ALU alu_block(.Ain(Ain), .Bin(Bin), .ALUop(ALUop), .out(out), .Z(Z)); 
   
   always_comb begin 
-    data_in = vsel ? datapath_in : datapath_out;   // if vsel is true, datapath_in is copied to data_in
-                                                   // else the output datapath_out is copied to data_in
+    // assign 0 for Lab 6
+    mdata = 16'b0;
+    PC = 8'b0; 
+    // change mux to 4-input (vsel changed to 2-bit value)
+    data_in = vsel[0] ? {vsel[1] ? C : {8'b0, PC}} : {vsel[1] ? sximm8 : mdata};   
+    // if vsel[0] is true
+          // if vsel[1] is true (vsel = 2'b11), C is copied to data_in
+          // if vsel[1] is false (vsel = 2'b10), {8'b0, PC} is copied to data_in
+    // if vsel[0] is false: 
+          // if vsel [1] is true (vsel = 2'b01), sximm8 is copied to data_in 
+          // if vesel [1] is fasle (vsel = 2'b00), mdata is copied to data_in
+    
     // copy values to Ain and Bin based on asel and bsel respectively 
     Ain = asel ? 16'b0 : aout;
-    Bin = bsel ? {11'b0, datapath_in[4:0]} : sout; 
+    Bin = bsel ? sximm5 : sout; 
   end
 
   always_ff @(posedge clk) begin 
