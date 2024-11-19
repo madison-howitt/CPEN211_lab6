4438277c7bc28a7e486de11d6a8be48315d66595
madison-howitt <159974224+madison-howitt@users.noreply.github.com>
2024-11-19 10:56:30 -0800

    Update alu.sv
    added negative and overflow flags 
    CHECK SYNTAX OF LINES 12 & 16 (should work but might not be the method they are looking for)

--- alu.sv
+++ alu.sv
@@ -1,27 +1,27 @@
-module ALU(Ain,Bin,ALUop,out,Z,N,O);
+module ALU(Ain,Bin,ALUop,out,Z,N,V);
   // declarations from Lab 5 outline
   input [15:0] Ain, Bin;
   input [1:0] ALUop;
   output reg [15:0] out;
-  output reg Z, N, O; // zero, negative, and overflow flags
+  output reg Z, N, V; // zero, negative, and overflow flags
 
   always_comb begin 
     case (ALUop) 
       2'b00: {
         out = Ain + Bin; 
-        O = ((Ain[15] == Bin[15]) && (out[15] != Ain[15])) ? 1 : 0;
+        V = ((Ain[15] == Bin[15]) && (out[15] != Ain[15])) ? 1 : 0;
       } // ^^^ THIS LINE MIGHT BE WRONG, SHOULD CHECK IN CONTEXT
       2'b01: {
         out = Ain + (~Bin + 1'b1);
-        O = ((Ain[15] == Bin[15]) && (out[15] != Ain[15])) ? 1 : 0;
+        V = ((Ain[15] == Bin[15]) && (out[15] != Ain[15])) ? 1 : 0;
       }
       2'b10: {
         out = Ain & Bin;
-        O = 0; 
+        V = 0; 
       } // overflow not possible for bitwise AND or bitwise NOT
       default: {
         out = ~Bin; 
-        O = 0;
+        V = 0;
       }
     endcase 
     Z = (out == 16'b0) ? 1 : 0;
